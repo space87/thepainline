fd2ac5c1a3917be28f2e9158f80489e7b43803d8
jason staerck <jason.staerck@moneysavingexpert.com>
2018-10-27 22:44:07 +0100

    added firebase

--- app/main.js
+++ app/main.js
@@ -1,12 +1,28 @@
 import Vue from 'nativescript-vue'
 import App from './components/App'
 import VueDevtools from 'nativescript-vue-devtools'
+import firebase from 'nativescript-plugin-firebase';
+
 
 if(TNS_ENV !== 'production') {
   Vue.use(VueDevtools)
 }
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
+
+
+firebase.init({
+  // Optionally pass in properties for database, authentication and cloud messaging,
+  // see their respective docs.
+}).then(
+ instance => {
+   console.log('firebase.init done');
+ },
+ error => {
+   console.log(`firebase.init error: ${error}`);
+ }
+);
+Vue.prototype.$firebase = firebase;
 
 
 new Vue({
